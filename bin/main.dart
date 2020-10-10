1ebe297c28d2630c55a4643495f248d9d764eb5f
Chris <chrissx@chrissx.de>
2020-10-10 22:10:16 +0200

    got a first pubspec poc working

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,14 @@
+import 'dart:io';
+
 import 'package:pub_api/pub_api.dart';
+import 'package:version/version.dart';
+import 'package:yaml/yaml.dart';
 
 main() async {
-  print(await PubPackage.fromName('schttp'));
+  final pubspec = loadYaml(await File('pubspec.yaml').readAsString());
+  final deps = pubspec['dependencies'];
+  for (final name in deps.keys)
+    if ((await PubPackage.fromName(name)).latest.version >
+        Version.parse(deps[name].replaceFirst('^', '')))
+      print(name + ' out of date');
 }
