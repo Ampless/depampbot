fb8a6fd49eed3119207f48f928fdb9cbfa8d6fe2
Chris <chrissx@chrissx.de>
2020-10-10 22:43:02 +0200

    started writing the git(hub) code

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:github/github.dart';
 import 'package:pub_api/pub_api.dart';
 import 'package:version/version.dart';
 import 'package:yaml/yaml.dart';
@@ -11,4 +12,20 @@ main() async {
     if ((await PubPackage.fromName(name)).latest.version >
         Version.parse(deps[name].replaceFirst('^', '')))
       print(name + ' out of date');
+  final github = GitHub();
+  final gitRemote =
+      (await Process.run('sh', ['-c', 'git remote get-url \$(git remote)']))
+          .stdout
+          .replaceAll(RegExp('[\r\n]'), '')
+          .replaceFirst(RegExp('(https?|git):\\/\\/github\\.com\\/'), '')
+          .replaceFirst(RegExp('\\.git\$'), '')
+          .split('/');
+  return;
+  github.issues.create(
+      RepositorySlug(gitRemote[0], gitRemote[1]),
+      IssueRequest(
+        title: 'depampbot dependency detected out of date',
+        body: 'The packages "bla" and "blabla" are out of date.',
+        labels: ['depampbot', 'enhancement'],
+      ));
 }
