1926af75cb5af8ab58f54e4fe230ac9ee0fb7b68
Chris <chrissx@chrissx.de>
2020-10-12 07:03:13 +0200

    put some stuff into functions

--- bin/main.dart
+++ bin/main.dart
@@ -5,26 +5,39 @@ import 'package:pub_api/pub_api.dart';
 import 'package:pub_semver/pub_semver.dart';
 import 'package:pubspec_parse/pubspec_parse.dart';
 
+Future<bool> versionIsOutdated(String name, VersionRange version) async =>
+    version.isAny ||
+    version.min < (await PubPackage.fromName(name)).latest.version;
+
+Future<bool> dependencyIsOutdated(String name, HostedDependency dep) =>
+    versionIsOutdated(name, dep.version);
+
+Future<String> getGitRemoteUrl() async {
+  final name = await Process.run('git', ['remote']);
+  final url = await Process.run('git',
+      ['remote', 'get-url', name.stdout.replaceAll(RegExp('[\r\n]'), '')]);
+  return url.stdout.replaceAll(RegExp('[\r\n]'), '');
+}
+
+Future<RepositorySlug> getGithubRepo() async {
+  final gitRemote = (await getGitRemoteUrl())
+      .replaceFirst(RegExp('(https?|git):\\/\\/github\\.com\\/'), '')
+      .replaceFirst(RegExp('\\.git\$'), '')
+      .split('/');
+  return RepositorySlug(gitRemote[0], gitRemote[1]);
+}
+
 main() async {
   final pubspec = Pubspec.parse(await File('pubspec.yaml').readAsString());
   final deps = pubspec.dependencies;
   for (final name in deps.keys)
-    if (deps[name] is HostedDependency &&
-            ((deps[name] as HostedDependency).version.isAny) ||
-        ((deps[name] as HostedDependency).version as VersionRange).min <
-            (await PubPackage.fromName(name)).latest.version)
+    if (await dependencyIsOutdated(name, deps[name]))
       print(name + ' out of date');
   final github = GitHub();
-  final gitRemote =
-      (await Process.run('sh', ['-c', 'git remote get-url \$(git remote)']))
-          .stdout
-          .replaceAll(RegExp('[\r\n]'), '')
-          .replaceFirst(RegExp('(https?|git):\\/\\/github\\.com\\/'), '')
-          .replaceFirst(RegExp('\\.git\$'), '')
-          .split('/');
+  final repo = await getGithubRepo();
   return;
   github.issues.create(
-      RepositorySlug(gitRemote[0], gitRemote[1]),
+      repo,
       IssueRequest(
         title: 'depampbot dependency detected out of date',
         body: 'The packages "bla" and "blabla" are out of date.',
